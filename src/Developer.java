ca22387147b885823090eaa06961689fffe78c34
sst8696 <sst8696@rit.edu>
2012-04-04 11:27:12 -0400

    More classes

---
+++ src/Developer.java
@@ -0,0 +1,9 @@
+public class Developer implements Runnable {
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/TeamLeader.java
@@ -0,0 +1,3 @@
+public class TeamLeader extends Developer {
+
+}
