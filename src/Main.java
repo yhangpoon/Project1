85b549f3259249e05ca3786a359bc6337f3b5ffd
Yin Poon <yhangpoon@gmail.com>
2012-04-11 10:50:08 -0400

    Updated Main

--- src/Main.java
+++ src/Main.java
@@ -15,9 +15,22 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        Simulator simulator = Simulator.getSimulator();
         Manager manager = new Manager();
-        Employee employee1 = new TeamLeader(manager);
+
+        TeamLeader leader1 = new TeamLeader(manager);
+        Developer developer1 = new Developer(leader1);
+        Developer developer2 = new Developer(leader1);
+        Developer developer3 = new Developer(leader1);
+
+        TeamLeader leader2 = new TeamLeader(manager);
+        Developer developer4 = new Developer(leader2);
+        Developer developer5 = new Developer(leader2);
+        Developer developer6 = new Developer(leader2);
+
+        TeamLeader leader3 = new TeamLeader(manager);
+        Developer developer7 = new Developer(leader3);
+        Developer developer8 = new Developer(leader3);
+        Developer developer9 = new Developer(leader3);
 
     }
 
