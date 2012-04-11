2df8beb4fdb5527b66ec6f07f2dc7cb0b0a1b546
peter <peter@192.168.1.101>
2012-04-11 11:17:52 -0400

    modified teamleader

--- src/TeamLeader.java
+++ src/TeamLeader.java
@@ -1,4 +1,7 @@
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 
 /**
  * This model describes a Team Leader. It extends Programmer.
@@ -17,25 +20,38 @@ public class TeamLeader extends Employee {
     /**
      * The Team Leader's project manager.
      */
-    private final Manager manager;
+    private Manager manager;
 
+    /**
+     * The team leader team of developers
+     */
+    private HashMap<Developer, Boolean> team = new HashMap<Developer, Boolean>();
+    
     /**
      * Default Constructor.
      * 
      * @param manager
      *            - The assigned project manager
      */
-    public TeamLeader(Manager manager, Calendar time) {
-        this.manager = manager;
+    public TeamLeader(Calendar time, List<Developer> devs) {
         this.time = time;
         this.arrived = false;
+        for (int i=0; i<devs.size(); i++){
+            this.team.put(devs.get(i), false);
+        }
     }
 
     /**
      * Answers Developer's question.
      */
     public void answerQuestion() {
-        // TODO
+        Random gen = new Random();
+        int askMan = gen.nextInt(2);
+        if (askMan%2 == 0){
+            this.manager.answerQuestion();
+        } else {
+            //TODO
+        }
     }
 
     /**
